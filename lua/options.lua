110684d09f8a24f8ba3398a913f5395f84347666
Shivang Patel <shivaang14@gmail.com>
2026-07-04 22:27:21 +0000

    add osc 52 support for remote development

--- lua/options.lua
+++ lua/options.lua
@@ -40,3 +40,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
+-- NVIM SSH COPY
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
+
